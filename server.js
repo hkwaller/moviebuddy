e5153524a62a560636e6deb3cee39b9451eef16b
Hannes Waller <hkwaller@gmail.com>
2015-03-12 22:37:34 +0100

    add serverside jwt

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var express =               require('express'),
+    expressJwt =            require('express-jwt'),
     app =                   express(),
     bodyParser =            require('body-parser'),
     mongoose =              require('mongoose'),
+    jwt =                   require('jsonwebtoken'),
     websocket =             require('./server/websockets.js'),
     movieController =       require('./server/controllers/movie-controller.js')
+    authController =       require('./server/controllers/auth-controller.js')
 
 
 mongoose.connect('mongodb://localhost:27017/movies')
 
-app.use('/', express.static(__dirname + '/webapp/www'))
+var secret = "supersecret"
 
+app.use('/api/', expressJwt({secret: secret}))
+app.use('/', express.static(__dirname + '/webapp/www'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/authenticate', authController.authenticate)
 app.post('/api/movies', movieController.create)
 app.get('/api/movies', movieController.list)
 app.delete('/api/movies', movieController.delete)
---
+++ server/controllers/auth-controller.js
@@ -0,0 +1,17 @@
+var jwt = require('jsonwebtoken')
+var secret = "supersecret"
+
+module.exports.authenticate = function(req, res) {
+    if (!(req.body.username === 'hannes' && req.body.password === 'password')) {
+        return;
+    }
+    
+    var user = {
+        first_name: 'Hannes',
+        last_name: 'Waller',
+        email: 'hkwaller@gmail.com'
+    };
+
+    var token = jwt.sign(user, secret, { expiresInMinutes: 60*5 });
+    res.json({ token: token });
+}
